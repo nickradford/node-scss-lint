21afd52711a24400a943ec25e876c470cdabc486
Nick Radford <nick.radford@software.dell.com>
2014-05-29 21:45:17 -0500

    Filling out the default scsslint.json file

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@
 module.exports = {
   'Is a function': function(test) {
     var scssLint = require('../lib/');
-    test.ok(typeof scssLint == 'function', 'node-scss-lint should be a function');
+    test.ok(typeof scssLint === 'function', 'node-scss-lint should be a function');
     test.done();
   }
 }
